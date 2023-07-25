f41864cb44405c7cb4b3d991269e722b54e83955
unknown <azuzochka@gmail.com>
2023-07-25 19:06:08 +0300

    Початковий стан

---
+++ popovychenko/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def about(request):
+    return httpresponse('This is information about the project')
+
+def home(request):
+    return render(request, 'home.html')
+
+def resume(request):
+    return render(request, 'resume.html')
